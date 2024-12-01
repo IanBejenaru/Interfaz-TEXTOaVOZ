a5323f52c58325a52ee5d61b6e0c61b9651a1254
IanBejenaru <ianbeje17@gmail.com>
2024-12-01 18:01:27 +0100

    cargando toda la interfaz, dando funcionalidad a los botones y mejorando los comentarios
    Se han mejorado los comentarios de las funciones y del código. Se ha dado funcionalidad a los principales botones.

--- Interfaz_TEXTOaVOZ.py
+++ Interfaz_TEXTOaVOZ.py
@@ -6,13 +6,22 @@
 
 # Importamos la librería para convertir texto a voz:
 import pyttsx3
+# Importamos la librería para crear una interfaz, poder cargar archivos abriendo el explorador de archivos y mostrar mensajes por pantalla:
+import tkinter as tk
+from tkinter import filedialog, messagebox
 
-# Creamos la función que va a inicializar el audio:
+# FUNCIONES
+
 def textoVoz(texto):
-    # INPUT
-    # texto: es el texto que se dirá por audio.
-    # OUTPUT: El sonido del texto.
+    """
+    Convierte un texto dado en audio utilizando la biblioteca pyttsx3.
 
+    INPUT:
+        texto (str): El texto que será convertido en audio.
+
+    OUTPUT:
+        El audio se reproduce directamente, no se retorna ningún valor.
+    """
     # Inicializamos el convertidor de texto a voz:
     engine = pyttsx3.init()
     # Le decimos lo que queremos que convierta a voz:
@@ -20,44 +29,112 @@ def textoVoz(texto):
     # Iniciamos el audio:
     engine.runAndWait()
 
+
+def execute_code():
+    """
+    Funcion que se ejecutará al presionar el primer botón 'LEER TEXTO ESCRITO'.
+    Se encargará de leer el contenido que esta escrito en el cuadro de escritura.
+
+    INPUT:
+        Guardará el contenido del cuadro de escritura en una variable.
+
+    OUTPUT:
+        Reproducirá el texto escrito en forma de audio, no se retorna ningún valor.
+    """
+    input_text = text_area.get("1.0", tk.END).strip()  # Obtener texto del área
+    # Si el area de texto existe, llamamos a la funcion textoaVoz() sino mostramos un mensaje de que no está rellenado el área.
+    if input_text:
+        # Llamamos a la función que nos va a permitir leer el texto
+        textoVoz(input_text)
+        # Aquí puedes agregar la lógica que deseas ejecutar
+    else:
+        messagebox.showwarning("Advertencia", "Por favor, escribe algo antes de ejecutar.")
+
+
+def open_file():
+    """
+    Funcion que se ejecutará al presionar el botón 'LEER TEXTO DE UN ARCHIVO'
+    y que se encargará de guardar la ruta de un archivo txt que quiera ser leido.
+
+    INPUT:
+        Guardará la ruta del archivo que se seleccione en una variable.
+
+    OUTPUT:
+        Llamará a la función leerLineasTXT si la ruta es correcta y
+        si la ruta es incorrecta, mostrará un mensaje de advertencia.
+    """
+    # Abrir el explorador de archivos para seleccionar un archivo
+    file_path = filedialog.askopenfilename(
+        title="Seleccionar un archivo",
+        filetypes=(("Archivos de texto", "*.txt"), ("Todos los archivos", "*.*"))
+    )
+    if file_path:
+        leerLineasTXT(file_path)
+    else:
+        messagebox.showwarning("Sin Selección", "No seleccionaste ningún archivo.")
+
+
 # Creamos la función que nos va a permitir leer lineas de un TXT:
 def leerLineasTXT(archivo):
-        # INPUT
-        # archivo: es el título del archivo que tenemos que leer.
-        # OUTPUT: El texto línea por línea.
+    """
+    Leerá las líneas que tiene un archivo txt. En caso de que el archivo no se encuentre saltará una excepción avisando al usuario.
 
-        # abrimos el archivo:
-        try: # Manejamos el error en caso de que cambie de nombre o de lugar el TXT.
-            book = open(archivo)
-            # Leemos el archivo entero y lo guardamos en una variable:
-            book_text= book.readlines()
-            # Creamos un bucle for que recorrerá línea por línea lo que tenemos
-            for line in book_text:
-                # Llamamos a la función que nos va a permitir leer el texto
-                textoVoz(line)
-        except FileNotFoundError:
-            print("El archivo no se encuentra")
+    INPUT:
+        archivo (str): Ruta del archivo txt que será convertido en audio.
+
+    OUTPUT:
+        El audio se reproduce línea por línea, no se retorna ningún valor.
+    """
+    # abrimos el archivo:
+    try: # Manejamos el error en caso de que cambie de nombre o de lugar el TXT.
+        book = open(archivo)
+        # Leemos el archivo entero y lo guardamos en una variable:
+        book_text= book.readlines()
+        # Creamos un bucle for que recorrerá línea por línea lo que tenemos
+        for line in book_text:
+            # Llamamos a la función que nos va a permitir leer el texto
+            textoVoz(line)
+    except FileNotFoundError: # Si el archivo no se encuentra, saltará esta advertencia
+        messagebox.showwarning("Advertencia", "El archivo no se encuentra")
+    except UnicodeDecodeError: # Si el archivo no es un txt, saltará esta advertencia
+        messagebox.showwarning("Advertencia", "El archivo no es un .txt")
 
 
-# Creamos un menú en el que el usuario pueda decidir si quiere escribir un texto para que se lea o que pille el texto de un archivo .txt 
-# variable que nos permite salir del bulce:
-salir = True
-while salir:
-    opcion=input("Elige una opción que quieres convertir a texto:\n1) Escribir texto y convertir a voz\n2) Leer texto de un archivo txt\n3) Salir\n")
+# Función que se activará cuando se cierre la ventana de la interfaz:
+def on_close():
+    """
+    Al presionarse la X para cerrar el programa, activará un audio para indicar que salimos del programa.
 
-    # El usuario escribe por pantalla el texto que quiere leer:
-    if opcion =="1":
-        # Creamos una variable en la que el usuario va a escribir un texto que quiera ser leído:
-        texto_usuario = input("Escribe un texto que quieres que se lea: ")
-        # Llamamos a la función que nos va a permitir leer el texto
-        textoVoz(texto_usuario)
-    elif opcion == "2":
-        # Pedimos al usuario que elija un archivo txt que quiere leer de su PC:
-        leerLineasTXT("archivoPrueba.txt")
-    elif opcion == "3":
-        # Llamamos a la función que nos va a permitir leer el texto
-        textoVoz("Saliendo del programa....")
-        salir = False
-    # Cuando se selecciona una opcion que no existe:
-    else:
-        print("La opcion seleccionada no existe, prueba otra vez")
+    INPUT:
+        Presionar la X para cerrar el programa.
+
+    OUTPUT:
+        Reproducirá un audio indicando el cierre del programa.
+    """
+    textoVoz("Saliendo del programa....")
+    root.destroy()  # Cerrar la ventana
+
+
+# CREAR VENTANA PRINCIPAL
+root = tk.Tk()
+root.title("Interfaz de Texto") # Título de la ventana
+root.geometry("400x260")  # Tamaño de la ventana
+root.resizable(False, False)  # Desactivar redimensionado
+# Conectar el evento de cierre a la función on_close
+root.protocol("WM_DELETE_WINDOW", on_close)
+
+# CREAR WIDGETS
+# Area de texto en el que se podrá escribir:
+text_area = tk.Text(root, wrap="word", height=5, font=("Arial", 12), borderwidth=2, relief="groove")
+# Diseño del primer botón, que nos permitirá leer lo que se ha escrito:
+execute_button = tk.Button(root, text="LEER TEXTO ESCRITO", command=execute_code, bg="#4caf50", fg="white", font=("Arial", 12), relief="raised")
+# Diseño del segundo botón, que nos permitirá leer lo que se ha escrito en un txt:
+button_two = tk.Button(root, text="LEER TEXTO DE UN ARCHIVO", command=open_file, bg="#2196f3", fg="white", font=("Arial", 12), relief="raised")
+
+# ORGANIZAR WIDGETS EN LA VENTANA:
+text_area.pack(pady=20, padx=20, fill="x")
+execute_button.pack(pady=10, fill="x", padx=50)
+button_two.pack(pady=5, fill="x", padx=50)
+
+# Iniciar el bucle principal de la aplicación
+root.mainloop()
